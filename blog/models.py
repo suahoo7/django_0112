cfc107624ed1343c3622d73c3d76f7dc6e3d0187
suahoo7 <hansua9977@gmail.com>
2023-01-12 10:49:25 +0900

    blog post change

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,14 @@ class Post(models.Model):
 # 데이터베이스의 필드(컬럼): 최대 길이가 30
     title = models.CharField(max_length=30)
     content = models.TextField()
-    created_at = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)    
+
 # author는 추후 작성
+
+    def __str__(self):
+        return f'[{self.pk}]_{self.title}' #[번호] 제목
+
+    # pk: 기본키로 레코드에 대한 고유한 값(중복 허용 안됨) 
+    # 포스트의 번호를 primary key로 지정
